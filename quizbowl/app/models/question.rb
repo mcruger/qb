4b6a5c50a03afebea68d98eacf8cba3a7f62b50f
Matt Cruger <mjcruger@gmail.com>
2014-03-01 16:27:02 -0600

    DB is seeding
    can seed db with questions and answers

--- quizbowl/app/models/question.rb
+++ quizbowl/app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
 	has_many :answers
-	belong_to :game
+	belongs_to :game
 	has_one :subject
 end
--- quizbowl/db/seeds.rb
+++ quizbowl/db/seeds.rb
@@ -32,8 +32,20 @@
 
 questions = [
 			 {:question => "In which sport would you use a chucker?"},
-			 {:question => "What would a tailor do with his hot plonker?"}
+			 {:question => "What would a tailor do with his hot plonker?"},
+			 {:question => "What is an alewife?"},
+			 {:question => "How many feet in a fathom?"},
+			 {:question => "What does a funambulist do?"}
 			]
+
+=begin
+			 {:question => "What did announcer Jack Brickhouse yell when the Cubs hit a home run?"},
+			 {:question => "How many rings on the Olympic flag?"},
+			 {:question => "Who was the first man in space?"},
+			 {:question => "What would a Scotsman do with a spurtle?"},
+			 {:question => "If you had caries who would you consult?"},
+=end
+
 
 Question.destroy_all
 
@@ -43,12 +55,8 @@ questions.each do |ques|
 	q.save
  end
 
-=begin
-	
-rescue Exception => e
-	
-end
-answers = [{:question_id => 1,
+
+answers = [{:question_id => 1,        #question 1
 			:answer => "Badminton",
 			:is_correct => 0},
 			{:question_id => 1,
@@ -56,11 +64,11 @@ answers = [{:question_id => 1,
 			:is_correct => 0},
 			{:question_id => 1,
 			:answer => "Cricket",
-			:is_correct => 1},
+			:is_correct => 1},         
 			{:question_id => 1,
 			:answer => "Badminton",
 			:is_correct => 0},
-			{:question_id => 2,
+			{:question_id => 2,      #question 2
 			:answer => "Suring up a seam",
 			:is_correct => 0},
 			{:question_id => 2,
@@ -71,16 +79,52 @@ answers = [{:question_id => 1,
 			:is_correct => 1},
 			{:question_id => 2,
 			:answer => "Stretching shrunken clothes",
-			:is_correct => 0}]
+			:is_correct => 0},
+			{:question_id => 3,      #question 3
+			:answer => "The wife of a brewer",
+			:is_correct => 0},
+			{:question_id => 3,
+			:answer => "A prairie flower",
+			:is_correct => 0},
+			{:question_id => 3,
+			:answer => "A small fish known to wash up on the beach",
+			:is_correct => 1},
+			{:question_id => 3,
+			:answer => "A nickname for a saloon dancer",
+			:is_correct => 0},
+			{:question_id => 4,       #question 4
+			:answer => "3.5",
+			:is_correct => 0},
+			{:question_id => 4,
+			:answer => "7",
+			:is_correct => 0},
+			{:question_id => 4,
+			:answer => "6",
+			:is_correct => 1},
+			{:question_id => 4,       
+			:answer => "10",
+			:is_correct => 0},
+			{:question_id => 5,       #question 5
+			:answer => "A pathogens specialist",
+			:is_correct => 0},
+			{:question_id => 5,
+			:answer => "A mechanic who works on very large engines",
+			:is_correct => 0},
+			{:question_id => 5,
+			:answer => "A tightrope walker",
+			:is_correct => 1},
+			{:question_id => 5,       
+			:answer => "A childrens birthday party coordinator",
+			:is_correct => 0}                  
+			]
 
 
 Answer.destroy_all
 
 answers.each do |ans|
     a = Answer.new
-    a.question_id = a[:question_id]
-    a.answer = a[:answer]
-    a.is_correct = a[:is_correct]
+    a.question_id = ans[:question_id]
+    a.answer = ans[:answer]
+    a.is_correct = ans[:is_correct]
 	a.save
  end
-=end
