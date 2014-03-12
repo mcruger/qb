40988118e7697258faf6ee1054e32cd70711a86a
Matt Cruger <mjcruger@gmail.com>
2014-03-12 15:35:40 -0500

    add in validation
    validated

--- quizbowl/app/models/user.rb
+++ quizbowl/app/models/user.rb
@@ -1,4 +1,48 @@
 class User < ActiveRecord::Base
+	before_validation(on: :create) do
+    	self.phone = phone.gsub(/[^0-9]/, "") if attribute_present?("phone")
+  	end
+
+  	before_validation(on: :update) do
+    	self.phone = phone.gsub(/[^0-9]/, "") if attribute_present?("phone")
+  	end
+
+  	before_validation(on: :create) do
+    	self.add_l1 = add_l1.gsub(/[^0-9A-Za-z\s]/, '') if attribute_present?("add_l1")
+  	end
+
+  	before_validation(on: :update) do
+    	self.add_l1 = add_l1.gsub(/[^0-9A-Za-z\s]/, '') if attribute_present?("add_l1")
+  	end
+
+	before_validation(on: :create) do
+    	self.first = first.gsub(/[^A-Za-z\s]/, '').titleize if attribute_present?("first")
+  	end
+
+  	before_validation(on: :update) do
+    	self.first = first.gsub(/[^A-Za-z\s]/, '').titleize if attribute_present?("first")
+  	end 
+
+  	before_validation(on: :create) do
+    	self.last = last.gsub(/[^A-Za-z\s]/, '').titleize if attribute_present?("last")
+  	end
+
+  	before_validation(on: :update) do
+    	self.last = last.gsub(/[^A-Za-z\s]/, '').titleize if attribute_present?("last")
+  	end
+
+  	before_validation(on: :create) do
+    	self.username = username.gsub(/[^A-Za-z\s]/, '').titleize if attribute_present?("username")
+  	end
+
+  	before_validation(on: :update) do
+    	self.username = username.gsub(/[^A-Za-z\s]/, '').titleize if attribute_present?("username")
+  	end 	
+
+  	validates :username, presence: true
+  	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }, presence: true
+  	validates :first, length: { maximum: 15 }, presence: true
+
 	has_many :players
 	has_many :provides, through: :players
 	
