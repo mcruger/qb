cb87a33152ec060a02fe1f8df04212ba50b1d78c
Matt Cruger <mjcruger@gmail.com>
2014-03-12 18:35:58 -0500

    maps working, users can join games

---
+++ quizbowl/app/controllers/players_controller.rb
@@ -0,0 +1,43 @@
+class PlayersController < ApplicationController
+
+	def index
+		
+	end
+
+
+	def new
+
+	end
+
+
+	def create
+		#create a new game in db
+		provide_id = params["provide_id"]
+		user_id = params["user_id"]
+    	p = Player.new
+		p.user_id = user_id
+		p.provide_id = provide_id    	
+    	p.save
+    	redirect_to show_lobbys_url
+	end
+
+
+	def edit
+
+
+
+	end
+
+
+
+	def update
+
+
+	end
+
+
+	def destroy
+
+	end
+
+end
